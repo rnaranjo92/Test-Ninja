a844c194ebbd1836c77aa1e8be22789f67bc34f7
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-03-26 20:11:58 -0700

    CanBeCancelledBy Test(My first test)

--- TestNinja.UnitTests/UnitTest1.cs
+++ TestNinja.UnitTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ReservationTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
+            //Arrange
+            var reservation = new Reservation();
+            // Act 
+
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+
+            // Assert
+            Assert.IsTrue(result);
+
         }
     }
 }
