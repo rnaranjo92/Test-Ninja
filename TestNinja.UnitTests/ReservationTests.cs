f3be74d912bc27f5029e9ca1a64ba223b4c0493b
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-03-26 20:46:50 -0700

    Added NUnit Framework

--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
-    [TestClass]
+    [TestFixture]
     public class ReservationTests
     {
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue()
         {
             //Arrange
@@ -16,11 +16,12 @@ namespace TestNinja.UnitTests
             var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
 
             // Assert
-            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
+
 
         }
 
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_SameUserCancellingtheReservation_ReturnsTrue()
         {
             var user = new User();
@@ -28,15 +29,16 @@ namespace TestNinja.UnitTests
 
             var result = reservation.CanBeCancelledBy(user);
 
-            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
         }
 
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_AnotherUserCancellingtheReservation_ReturnsFalse()
         {
             var reservation = new Reservation { MadeBy = new User()};
             var result = reservation.CanBeCancelledBy(new User());
-            Assert.IsFalse(result);
+
+            Assert.That(result, Is.False);
         }
     }
 }
