463c134451e649c34b9fd6db2ebd70a44e1a5512
Rolando Naranjo <rolando.naranjo92@yahoo.com>
2019-03-28 21:45:29 -0700

    Demerit points calculation tests

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        private DemeritPointsCalculator _demeritPointsCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _demeritPointsCalculator = new DemeritPointsCalculator();
+        }
+        [Test]
+        public void CalculateDemeritPoints_SpeedLimitBelow65_ReturnZero()
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(55);
+
+            Assert.That(result, Is.EqualTo(0));
+        }
+        [Test]
+        public void CalculateDemeritPoints_OverTheSpeedLimit_ReturnDemeritPoints()
+        {
+            var result = _demeritPointsCalculator.CalculateDemeritPoints(70);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+        [Test]
+        public void CalculateDemeritPoints_OverMaxSpeed_ReturnArgumentOutOfRange()
+        {
+            Assert.That(()=> _demeritPointsCalculator.CalculateDemeritPoints(305), Throws.InstanceOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
